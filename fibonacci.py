d27238f020ff46a3b32738d46b1d36e758ac9326
Abinav-Ramjee123 <aramjee@uoguelph.ca>
2025-10-24 09:02:38 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,25 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+def get_user_input():
+    num = int(input("How many terms? "))
+    return num
+
+def generate_fibonacci(n):
+    a, b = 0, 1
+    seq = []
+    for _ in range(n):
+        seq.append(a)
+        a, b = b, a + b
+    return seq
+
+def print_sequence(seq):
+    for num in seq:
+        print(num)
+
+def main():
+    n = get_user_input()
+    fib = generate_fibonacci(n)
+    print_sequence(fib)
+
+main()
